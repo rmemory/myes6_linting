952903e5291ad491d9a2c80ec6f336c931dceaa3
Richard Memory <richardfmemory@gmail.com>
2018-05-22 14:55:44 -0600

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "browser": true
+    },
+    // "extends": "eslint:recommended",
+    "extends": "airbnb",
+    "rules": {
+        "no-console": [
+            "warn",
+
+        ],
+        "indent": [
+            "warn",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
